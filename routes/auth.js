ad041b1bf574bd392f783e7bd96ec4a64de0ffce
Yasir45455 <ym5480012@gmail.com>
2025-12-18 10:56:21 +0500

    Sdjsdjfjlsdf

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,13 @@
+// import express from "express";
+// import { signup, verifyOTP, login } from "../controllers/authController.js";
 
+// const router = express.Router();
+
+// router.post("/signup", signup);
+// router.post("/verify-otp", verifyOTP);
+// router.post("/login", login);
+
+// export default router;
 
 
 import express from "express";
